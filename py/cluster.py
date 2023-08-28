82fcc1aeaafcd12c1f06e789ec0205f5beaf6db5
Lev Stambler <levstamb@gmail.com>
2023-08-28 14:58:10 -0400

    clustersize

--- py/cluster.py
+++ py/cluster.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+from langchain import LLMChain, OpenAI, PromptTemplate
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
@@ -34,3 +35,16 @@ def cluster(embeddings: Embeddings, n_clusters: int, dimensions=2):
     u_labels = np.unique(label)
     return df, label, u_labels
 
+
+def relative_labels(thms1: List[str], thms2: List[str], centroid_idx1: int, centroid_idx2: int, api_key=None) -> str:
+    llm = OpenAI(model_name="gpt-4", openai_api_key=api_key)
+    template = """Given the following two labeled sets of Lean theorems, can you describe the main difference in one sentence?
+
+Set {label1}: "{set1}"
+
+Set {label2}: "{set2}"
+"""
+    prompt = PromptTemplate(template=template, input_variables=["set1", "set2", "label1", "label2"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    return llm_chain.run(set1="\n".join(thms1), set2="\n".join(thms2), label1=f"Cluster {centroid_idx1}", label2=f"Cluster {centroid_idx2}")
+    
